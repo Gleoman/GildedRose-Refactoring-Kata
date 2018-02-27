ea9af44583b3d1390d8264ca8f7e7e4bdd1939be
Ed Goold <teddy_goold@hotmail.com>
2018-02-27 15:42:57 +0000

    empty items spec created - brie and sulfuras tests developed while remaining with GR spec - CI included

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -29,6 +29,12 @@ describe GildedRose do
 				GildedRose.new(std_items).update_quality()
 				expect(std_items[0].quality).to eq 8
 			end
+
+			it "cannot allow an item's quality to be negative" do
+				std_items = [Item.new("foo", 0, 0)]
+				GildedRose.new(std_items).update_quality()
+				expect(std_items[0].quality).to eq 0
+			end
 		end
 
 		context "when dealing with Aged Brie" do
@@ -36,6 +42,26 @@ describe GildedRose do
 				brie = [Item.new("Aged Brie", 10, 10)]
 				GildedRose.new(brie).update_quality()
 				expect(brie[0].quality).to eq 11
+			end
+
+			it "cannot increase in quality above 50" do
+				brie = [Item.new("Aged Brie", 10, 50)]
+				GildedRose.new(brie).update_quality()
+				expect(brie[0].quality).to eq 50
+			end
+		end
+
+		context "when dealing with Sulfuras" do
+			it "does not decrease in sell_in value" do
+				sulfuras = [Item.new("Sulfuras, Hand of Ragnaros", 1, 50)]
+				GildedRose.new(sulfuras).update_quality()
+				expect(sulfuras[0].sell_in).to eq 1
+			end
+
+			it "does not decrease in quality" do
+				sulfuras = [Item.new("Sulfuras, Hand of Ragnaros", 1, 50)]
+				GildedRose.new(sulfuras).update_quality()
+				expect(sulfuras[0].quality).to eq 50
 			end
 		end
 	end
---
+++ spec/items_spec.rb
@@ -0,0 +1,8 @@
+require 'rspec'
+require 'gilded_rose.rb'
+require 'items.rb'
+require 'pry'
+
+describe Item do
+
+end

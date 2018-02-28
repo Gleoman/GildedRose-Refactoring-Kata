37d4513141d3f1c03c4de09bee65e890e63c5ac1
Ed Goold <teddy_goold@hotmail.com>
2018-02-28 18:00:01 +0000

    refactored out a new class - quality evaluator - which performs much of the business logic regarding the quality update of the items

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,69 +1,26 @@
 require 'items'
+require 'qualityevaluator'
 
 class GildedRose
-  def initialize(items)
+  def initialize(items, quality_evaluator)
     @items = items
+    @quality_evaluator = quality_evaluator
   end
 
   def update_quality
     @items.each do |item|
-			case item.name
-			when "Aged Brie"
-				brie(item)
-			when "Backstage passes to a TAFKAL80ETC concert"
-				pass(item)
-			when "Sulfuras, Hand of Ragnaros"
-				item
-			when "Conjured item"
-				conjured(item)
-			else
-				standard_stock(item)
-			end
-		end
-	end
-
-	private
-
-	def brie(item)
-		item.quality += 1 if item.sell_in > 0
-		quality(item)
-		item.sell_in -= 1
-	end
-
-INF = 1.0/0.0
-
-	def pass(item)
-		case item.sell_in
-		when 11..INF
-			item.quality += 1
-		when 6..10
-			item.quality += 2
-		when 1..5
-			item.quality += 3
-		else
-			item.quality = 0
-		end
-		quality(item)
-		item.sell_in -= 1
-	end
-
-	def standard_stock(item)
-		item.sell_in > 0 ? item.quality -= 1 : item.quality -= 2
-		quality(item)
-		item.sell_in -= 1
-	end
-
-	def conjured(item)
-		item.sell_in > 0 ? item.quality -= 2 : item.quality -= 4
-		quality(item)
-		item.sell_in -= 1
-	end
-
-	def quality(item)
-		if item.quality > 50
-			item.quality = 50
-		elsif item.quality < 1
-			item.quality = 0
-		end
-	end
+      case item.name
+      when "Aged Brie"
+        @quality_evaluator.brie(item)
+      when "Backstage passes to a TAFKAL80ETC concert"
+        @quality_evaluator.pass(item)
+      when "Sulfuras, Hand of Ragnaros"
+        item
+      when "Conjured item"
+        @quality_evaluator.conjured(item)
+      else
+        @quality_evaluator.standard_stock(item)
+      end
+    end
+  end
 end
--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -2,35 +2,38 @@ require 'rspec'
 require 'gilded_rose.rb'
 
 describe GildedRose do
+
+	quality_evaluator = QualityEvaluator.new
+
 	describe "#update_quality" do
     it "does not change the name" do
 			std_items = [Item.new("foo", 10, 10)]
-      GildedRose.new(std_items).update_quality
+      GildedRose.new(std_items, quality_evaluator).update_quality
       expect(std_items[0].name).to eq "foo"
     end
 
 		context "when dealing with standard stock" do
 			it "reduces the quality of the item by 1" do
 				std_items = [Item.new("Standard stock", 10, 10)]
-				GildedRose.new(std_items).update_quality
+				GildedRose.new(std_items, quality_evaluator).update_quality
 				expect(std_items[0].quality).to eq 9
 			end
 
 			it "reduces the sell_in value of the item by 1" do
 				std_items = [Item.new("Standard stock", 10, 10)]
-				GildedRose.new(std_items).update_quality
+				GildedRose.new(std_items, quality_evaluator).update_quality
 				expect(std_items[0].sell_in).to eq 9
 			end
 
 			it "once sell_in date passed, it decreases the quality of an item by a reduction of 2" do
 				std_items = [Item.new("Standard stock", 0, 10)]
-				GildedRose.new(std_items).update_quality
+				GildedRose.new(std_items, quality_evaluator).update_quality
 				expect(std_items[0].quality).to eq 8
 			end
 
 			it "cannot allow an item's quality to be negative" do
 				std_items = [Item.new("Standard stock", 0, 0)]
-				GildedRose.new(std_items).update_quality
+				GildedRose.new(std_items, quality_evaluator).update_quality
 				expect(std_items[0].quality).to eq 0
 			end
 		end
@@ -38,13 +41,13 @@ describe GildedRose do
 		context "when dealing with Aged Brie" do
 			it "increases in quality as its sell_in value decreases" do
 				brie = [Item.new("Aged Brie", 10, 10)]
-				GildedRose.new(brie).update_quality
+				GildedRose.new(brie, quality_evaluator).update_quality
 				expect(brie[0].quality).to eq 11
 			end
 
 			it "cannot increase in quality above 50" do
 				brie = [Item.new("Aged Brie", 10, 50)]
-				GildedRose.new(brie).update_quality
+				GildedRose.new(brie, quality_evaluator).update_quality
 				expect(brie[0].quality).to eq 50
 			end
 		end
@@ -52,13 +55,13 @@ describe GildedRose do
 		context "when dealing with Sulfuras" do
 			it "does not decrease in sell_in value" do
 				sulfuras = [Item.new("Sulfuras, Hand of Ragnaros", 1, 80)]
-				GildedRose.new(sulfuras).update_quality
+				GildedRose.new(sulfuras, quality_evaluator).update_quality
 				expect(sulfuras[0].sell_in).to eq 1
 			end
 
 			it "does not decrease in quality" do
 				sulfuras = [Item.new("Sulfuras, Hand of Ragnaros", 1, 80)]
-				GildedRose.new(sulfuras).update_quality
+				GildedRose.new(sulfuras, quality_evaluator).update_quality
 				expect(sulfuras[0].quality).to eq 80
 			end
 		end
@@ -66,56 +69,56 @@ describe GildedRose do
 		context "when dealing with backstage passes" do
 			it "increases in quality by 1 when 11 days or more of sell_in value left" do
 				backstage_pass = [Item.new("Backstage passes to a TAFKAL80ETC concert", 11, 10)]
-				GildedRose.new(backstage_pass).update_quality
+				GildedRose.new(backstage_pass, quality_evaluator).update_quality
 				expect(backstage_pass[0].quality).to eq 11
 			end
 
 			it "increases in quality by 2 when 10 days of sell_in value left" do
 				backstage_pass = [Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 10)]
-				GildedRose.new(backstage_pass).update_quality
+				GildedRose.new(backstage_pass, quality_evaluator).update_quality
 				expect(backstage_pass[0].quality).to eq 12
 			end
 
 			it "increases in quality by 2 when 6 days of sell_in value left" do
 				backstage_pass = [Item.new("Backstage passes to a TAFKAL80ETC concert", 6, 10)]
-				GildedRose.new(backstage_pass).update_quality
+				GildedRose.new(backstage_pass, quality_evaluator).update_quality
 				expect(backstage_pass[0].quality).to eq 12
 			end
 
 			it "increases in quality by 3 when 5 days of sell_in value left" do
 				backstage_pass = [Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 10)]
-				GildedRose.new(backstage_pass).update_quality
+				GildedRose.new(backstage_pass, quality_evaluator).update_quality
 				expect(backstage_pass[0].quality).to eq 13
 			end
 
 			it "increases in quality by 3 when 1 day of sell_in value left" do
 				backstage_pass = [Item.new("Backstage passes to a TAFKAL80ETC concert", 1, 10)]
-				GildedRose.new(backstage_pass).update_quality
+				GildedRose.new(backstage_pass, quality_evaluator).update_quality
 				expect(backstage_pass[0].quality).to eq 13
 			end
 
 			it "cannot increase in quality above 50 when incrementing by units of 1" do
 				backstage_pass = [Item.new("Backstage passes to a TAFKAL80ETC concert", 11, 50)]
-				GildedRose.new(backstage_pass).update_quality
+				GildedRose.new(backstage_pass, quality_evaluator).update_quality
 				expect(backstage_pass[0].quality).to eq 50
 			end
 
 			it "cannot increase in quality above 50 when incrementing by units of 2" do
 				backstage_pass = [Item.new("Backstage passes to a TAFKAL80ETC concert", 6, 49)]
-				GildedRose.new(backstage_pass).update_quality
+				GildedRose.new(backstage_pass, quality_evaluator).update_quality
 				expect(backstage_pass[0].quality).to eq 50
 			end
 
 
 			it "cannot increase in quality above 50 when incrementing by units of 3" do
 				backstage_pass = [Item.new("Backstage passes to a TAFKAL80ETC concert", 1, 49)]
-				GildedRose.new(backstage_pass).update_quality
+				GildedRose.new(backstage_pass, quality_evaluator).update_quality
 				expect(backstage_pass[0].quality).to eq 50
 			end
 
 			it "decreases in quality to 0 when sell_in value reaches 0" do
 				backstage_pass = [Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 50)]
-				GildedRose.new(backstage_pass).update_quality
+				GildedRose.new(backstage_pass, quality_evaluator).update_quality
 				expect(backstage_pass[0].quality).to eq 0
 			end
 		end
@@ -123,25 +126,25 @@ describe GildedRose do
 		context "when dealing with conjured items" do
 			it "reduces the quality of the item by 2" do
 				std_items = [Item.new("Conjured item", 10, 10)]
-				GildedRose.new(std_items).update_quality
+				GildedRose.new(std_items, quality_evaluator).update_quality
 				expect(std_items[0].quality).to eq 8
 			end
 
 			it "reduces the sell_in value of the item by 1" do
 				std_items = [Item.new("Conjured item", 10, 10)]
-				GildedRose.new(std_items).update_quality
+				GildedRose.new(std_items, quality_evaluator).update_quality
 				expect(std_items[0].sell_in).to eq 9
 			end
 
 			it "once sell_in date passed, it decreases the quality of an item by a reduction of 4" do
 				std_items = [Item.new("Conjured item", 0, 10)]
-				GildedRose.new(std_items).update_quality
+				GildedRose.new(std_items, quality_evaluator).update_quality
 				expect(std_items[0].quality).to eq 6
 			end
 
 			it "cannot allow an item's quality to be negative" do
 				std_items = [Item.new("Conjured item", 0, 0)]
-				GildedRose.new(std_items).update_quality
+				GildedRose.new(std_items, quality_evaluator).update_quality
 				expect(std_items[0].quality).to eq 0
 			end
 		end
